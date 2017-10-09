885b390bf75cc6636b1b6c3116c4d356249c2d45
DiogoTakayama <diogo.takayamas@gmail.com>
2017-10-09 20:05:48 -0300

    Create FibonacciRecursivo.c

---
+++ FibonacciRecursivo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int fibo(int n)
+{
+    if (n<=1){
+
+    return n;
+    }
+    else{
+
+    return fibo(n-1)+fibo(n-2);
+    }
+}
+
+int main()
+{
+	int n;
+
+	scanf("%d", &n);
+
+	printf("\nO fibonacci eh: %d\n",  fibo(n));
+
+	return 0;
+}
